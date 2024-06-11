b1e763a25fe5192039378d80b1de3e0b73ad925e
feroz-hub <bashaferoz66@gmail.com>
2024-06-11 21:12:44 +0530

    Initial

---
+++ MqttMainScreen/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MqttMainScreen.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
