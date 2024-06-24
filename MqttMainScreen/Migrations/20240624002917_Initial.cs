1a57840c057079161c5dc09b017468b5e36206ad
feroz-hub <bashaferoz66@gmail.com>
2024-06-24 06:00:14 +0530

    DB Update

---
+++ MqttMainScreen/Migrations/20240624002917_Initial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MqttMainScreen.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MqttClientRegistrations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    SubscribedTopics = table.Column<string>(type: "TEXT", nullable: false),
+                    LastConnected = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    LastDisconnected = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    IsConnected = table.Column<bool>(type: "INTEGER", nullable: false),
+                    KeepAlive = table.Column<int>(type: "INTEGER", nullable: false),
+                    IpAddress = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MqttClientRegistrations", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MqttClientRegistrations");
+        }
+    }
+}
