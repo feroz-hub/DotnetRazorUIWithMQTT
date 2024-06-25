d31fb03e33f6b7c1880102e509c8ed593cf8fdd4
feroz-hub <bashaferoz66@gmail.com>
2024-06-25 22:42:38 +0530

    UI Change

--- MqttMainScreen/Commands/LogRequestMessageCommand.cs
+++ MqttMainScreen/Commands/LogRequestMessageCommand.cs
@@ -8,11 +8,9 @@ public class LogRequestMessageCommand(MqttLogRequest mqttLogRequest):IRequest<bo
     public MqttLogRequest LogRequest => new()
     {
         RequestId = Guid.NewGuid(),
-        ActionType = mqttLogRequest.ActionType,
-        IsAckRequired = mqttLogRequest.IsAckRequired,
-        LogRequestDto = mqttLogRequest.LogRequestDto,
         RequestDate = DateTime.Now,
         SourceId = mqttLogRequest.SourceId,
-        TargetId = mqttLogRequest.TargetId
+        TargetId = mqttLogRequest.TargetId,
+        LogRequestDto = mqttLogRequest.LogRequestDto,
     };
 }
--- MqttMainScreen/Models/LogRequestModel.cs
+++ MqttMainScreen/Models/LogRequestModel.cs
@@ -1,36 +1,23 @@
 namespace MqttMainScreen.Models;
 
-public class LogRequestModel
-
-{
-   public Guid RequestId { get; set; }
-    public string TargetId { get; init; }
-    public string SourceId { get; init; }
-    public ActionType ActionType { get; init; }
-    public DateTime RequestDate { get; set; }
-    public bool IsAckRequired { get; init; }
-    public ActionValues ActionValues { get; init; }
-    public ResponseType ResponseType { get; init; }
-    public DateTime FromDate { get; set; }
-    public DateTime EndDate { get; set; }
-}
 
 public class MqttLogRequest
 {
     public Guid RequestId { get; set; }
-    public string TargetId { get; init; }
-    public string SourceId { get; init; }
-    public ActionType ActionType { get; init; }
+    public string TargetId { get; init; }=default!;
+    public string SourceId { get; init; } = default!;
     public DateTime RequestDate { get; set; }
-    public bool IsAckRequired { get; init; }
-    public LogRequestDto LogRequestDto { get; set; }
+    public LogRequestDto LogRequestDto { get; init; } = default!;
 }
 public class LogRequestDto
 {
     public List<LogType>  LogTypes { get; init; }=default!;
     public List<LogLevel> LogLevels { get; init; }=default!;
-    public DateTime FromDate { get; set; }
-    public DateTime EndDate { get; set; }
+    public bool IsAckRequired { get; init; } 
+    public ActionType ActionType { get; init; }
+    public ResponseType ResponseType { get; init; }
+    public DateTime FromDate { get; init; }
+    public DateTime EndDate { get; init; }
 }
 public enum ResponseType
 {
